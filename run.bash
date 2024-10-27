0ced9ca5e5196b3dbfd6a679cee3c05f2f91d106
Matej Pelusic <matejpelusic@gmail.com>
2024-10-27 14:56:42 +0000

    Run the website

--- run.bash
+++ run.bash
@@ -10,3 +10,9 @@ fi
 # Run the Docker container with port mapping
 echo "Running Docker container..."
 docker run -d -p 5000:5000 --name website-container my-website
+
+# Scrape the local IPv4 address
+LOCAL_IPV4=$(hostname -I | awk '{print $1}')
+
+# Output the address to access the website
+echo "Website is running at http://$LOCAL_IPV4:5000"
