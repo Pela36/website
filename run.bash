148b290d909206611d54eab10f2d441a5d508e1d
Matej Pelusic <matejpelusic@gmail.com>
2024-10-27 14:43:26 +0000

    Add install.bash for building the image and update run.bash to only run the container

--- run.bash
+++ run.bash
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Build the Docker image
-docker build -t my-website .
-
 # Stop and remove any existing container with the same name
 if [ "$(docker ps -aq -f name=website-container)" ]; then
     echo "Stopping and removing existing container..."
@@ -11,4 +8,5 @@ if [ "$(docker ps -aq -f name=website-container)" ]; then
 fi
 
 # Run the Docker container with port mapping
+echo "Running Docker container..."
 docker run -d -p 5000:5000 --name website-container my-website
